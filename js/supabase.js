0390f337da5c4ddc22ace9d5fbbae80d6c10a9e6
zeyad3002 <alanesyzeyad@gmail.com>
2025-09-16 01:09:28 +0300

    35 edite

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,9 @@
+// js/supabase.js
+
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 const supabase = createClient(
   process.env.SUPABASE_DATABASE_URL,
   process.env.SUPABASE_ANON_KEY
 );
+export default supabase;
