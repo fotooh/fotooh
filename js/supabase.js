d805e5bd23fa4e30ccc8069fef67bc9f623580e1
zeyad3002 <alanesyzeyad@gmail.com>
2025-09-17 05:04:12 +0300

    43 edite

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,5 @@
-// supabase.js - النسخة المعدلة
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm"
-
-const supabaseUrl = 'https://iwbknenqfyvvtugnsxic.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3YmtuZW5xZnl2dnR1Z25zeGljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNTQ2NjksImV4cCI6MjA2OTkzMDY2OX0.3-IrAdrgRBoPoG8B94zoL-ctUpFfkSioefFWVchPSV4'
-
-// إنشاء العميل وتصديره مباشرة
-export const supabase = createClient(supabaseUrl, supabaseKey)
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+const supabaseUrl = 'https://iwbknenqfyvvtugnsxic.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3YmtuZW5xZnl2dnR1Z25zeGljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNTQ2NjksImV4cCI6MjA2OTkzMDY2OX0.3-IrAdrgRBoPoG8B94zoL-ctUpFfkSioefFWVchPSV4';
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase
